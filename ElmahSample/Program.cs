fd8f0ec91695dbab446f6e4a811bf3ce87d5813d
Weiyi Lai <joy777park@gmail.com>
2020-06-30 00:15:33 +0800

    加入專案檔案。

---
+++ ElmahSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Elmah;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElmahSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Convert.ToInt32("A");
+            }
+            catch (Exception ex)
+            {
+                ErrorLog errorLog = ErrorLog.GetDefault(null);
+                errorLog.Log(new Error(ex));
+            }
+        }
+    }
+}
